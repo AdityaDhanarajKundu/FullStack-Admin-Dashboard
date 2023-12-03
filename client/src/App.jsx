c2b9b7eec957f85906840668919c8765cd9397a8
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-03 19:33:36 +0530

    retrieving the state data value(mode) from the redux toolkit store and passing it to createTheme function to get the theme and at the same time using useMemo to memoize the theme and is invoked every time the state value of the mode changes by passing the state variable in the dependency array of useMemo hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,14 @@
+import {CssBaseline, ThemeProvider} from '@mui/material';
+import {createTheme} from '@mui/material/styles';
+import { useMemo } from 'react';
+import {useSelector} from 'react-redux'; // hook function to select and access the state data from the redux store
+import { themeSettings } from 'theme';
 
 function App() {
 
+  const mode = useSelector((store)=> store.global.mode); // access the state data from the redux store
+  const theme = useMemo(()=> createTheme(themeSettings(mode)), [mode]); // create a theme object based on the mode state data
+  
   return (
     <div className="app">
       <h1>App</h1>
