41644548466f4501c06bb7d66710441b116263fc
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-03 19:02:47 +0530

    a redux toolkit slice to manage/toggle the light mode of our application

---
+++ client/src/state/index.js
@@ -0,0 +1,23 @@
+// file to create a redux toolkit slice for the state management of light/dark mode
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    mode: "dark",
+};
+
+export const globalSlice = createSlice({
+    name: "global",
+    initialState,
+    reducers: {
+        setMode: (state) => { // reducer function to toggle the light mode
+            state.mode = state.mode === "light" ? "dark" : "light";
+        },
+    },
+})
+
+// destructuring the reducer function to export it
+export const { setMode } = globalSlice.actions;
+
+// exporting the reducer property of the stateSlice to be used in the store
+export default globalSlice.reducer;
