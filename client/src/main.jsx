d4f9b1600508f6373c5983ff4840ba850f5c7a05
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-03 19:18:56 +0530

    redux toolkit boilerplate setup in the main jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import globalReducer from "state";   // the reducer property of the globalSlice
+import { configureStore} from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+// creating the redux toolkit store
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
