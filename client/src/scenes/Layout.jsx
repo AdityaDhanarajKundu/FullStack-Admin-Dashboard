62fce5066316a03dcb1525bb7afd86fa43e2d435
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-06 13:31:05 +0530

    making the basic layout element

--- client/src/scenes/Layout.jsx
+++ client/src/scenes/Layout.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+import {Box, useMediaQuery} from "@mui/material";
+import {Outlet} from "react-router-dom";
+import {useSelector} from "react-redux";
+import Navbar from "../components/Navbar";
+
 function Layout(){
     return(
-        <div>
-            <h1>Layout</h1>
-        </div>
+        <Box width={"100%"} height={"100%"}>
+            <Box>
+                <Navbar />
+                <Outlet />
+            </Box>
+        </Box>
     );
 }
 
