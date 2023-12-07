f20d763529051f562ea667b865c06ee236d8950d
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-07 18:32:34 +0530

    passed the sidebar state values and the sidebar state function to the navbar so that we are able to toggle open and close sidebar from the menu icon in the navbar.

--- client/src/scenes/Layout.jsx
+++ client/src/scenes/Layout.jsx
@@ -2,13 +2,28 @@ import { useState } from "react";
 import {Box, useMediaQuery} from "@mui/material";
 import {Outlet} from "react-router-dom";
 import {useSelector} from "react-redux";
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 function Layout(){
+    const isNonMobile = useMediaQuery("(min-width: 600px)"); 
+
+    // state values to toggle the sidebar 
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    
     return(
-        <Box width={"100%"} height={"100%"}>
+        <Box display={isNonMobile ? "flex" : "block"} width={"100%"} height={"100%"}>
+            <Sidebar 
+                isNonMobile={isNonMobile}
+                drawerWidth="250px"
+                isSidebarOpen={isSidebarOpen}
+                setIsSidebarOpen={setIsSidebarOpen}
+            />
             <Box>
-                <Navbar />
+                <Navbar 
+                    isSidebarOpen={isSidebarOpen}
+                    setIsSidebarOpen={setIsSidebarOpen}
+                />
                 <Outlet />
             </Box>
         </Box>
