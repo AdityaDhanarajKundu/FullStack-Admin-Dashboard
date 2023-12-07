7c320572abbbb23693fcabb8afa9c4e0fb24b18d
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-07 21:54:48 +0530

    added a userId as a new state value in the global state reducer in the store

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -4,6 +4,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     mode: "dark",
+    userId: "63701cc1f03239b7f700000e", // default user id(considering user already logged in with this user id)
 };
 
 export const globalSlice = createSlice({
