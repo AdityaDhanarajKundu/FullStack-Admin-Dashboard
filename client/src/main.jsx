695cc26091b02247845dcd29902659207aa9f821
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-08 15:15:17 +0530

    setting up the redux toolkit store for the api query

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,16 +5,18 @@ import './index.css';
 import globalReducer from "./state/index.js";   // the reducer property of the globalSlice
 import { configureStore} from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
+import {setupListeners} from '@reduxjs/toolkit/query';
+import {api} from "./state/api.js";
 
 // creating the redux toolkit store
 const store = configureStore({
   reducer: {
     global: globalReducer,
-  }
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefault)=> getDefault().concat(api.middleware),
 });
+setupListeners(store.dispatch);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
