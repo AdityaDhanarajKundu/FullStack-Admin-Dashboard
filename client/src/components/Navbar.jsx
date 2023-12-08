c82915ad51ec699ced194da3c50de2fb6ca5e536
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-08 21:35:00 +0530

    added the user details from the user data that we fetched from the database making an api call to the backend into a Button component onclick on which it will display a dropdown menu component with a menuitem component of Log Out

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,17 +10,36 @@ import FlexBetween from "./FlexBetween";
 import { useDispatch } from "react-redux";
 import { setMode } from "../state/index";
 import profileImage from "../assets/unsplash.jpg";
+import ogImage from "../assets/profile.jpeg";
 import {
   AppBar,
   Toolbar,
   IconButton,
   InputBase,
   useTheme,
+  Button,
+  Box,
+  Typography,
+  Menu,
+  MenuItem,
 } from "@mui/material";
+import { useState } from "react";
+import PropTypes from "prop-types";
 
-function Navbar({ isSidebarOpen, setIsSidebarOpen}) {
+function Navbar({ user, isSidebarOpen, setIsSidebarOpen }) {
   const dispatch = useDispatch();
   const theme = useTheme();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isOpen = Boolean(anchorEl);
+
+  function handleClick(event) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
 
   return (
     <AppBar
@@ -38,9 +57,9 @@ function Navbar({ isSidebarOpen, setIsSidebarOpen}) {
           </IconButton>
           <FlexBetween
             backgroundColor={theme.palette.background.alt}
-            borderRadius={"9px"}
-            gap={"3rem"}
-            padding={"0.1rem 1.5rem"}
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1rem 1.5rem"
           >
             <InputBase placeholder="Search..." />
             <IconButton>
@@ -51,20 +70,66 @@ function Navbar({ isSidebarOpen, setIsSidebarOpen}) {
 
         {/* Right side of the navbar */}
         <FlexBetween gap={"1.5rem"}>
-            <IconButton onClick={()=>dispatch(setMode())}>
-                {theme.palette.mode === "dark" ? (
-                    <DarkModeOutlined sx={{fontSize: "25px"}} />
-                ) : (
-                    <LightModeOutlined sx={{fontSize: "25px"}} />
-                )}
-            </IconButton>
-            <IconButton>
-                <SettingsOutlined sx={{fontSize: "25px"}} />
-            </IconButton>
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlined sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: "25px" }} />
+            )}
+          </IconButton>
+
+          <IconButton>
+            <SettingsOutlined sx={{ fontSize: "25px" }} />
+          </IconButton>
+
+          <FlexBetween>
+            <Button
+              onClick={handleClick}
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                gap: "1rem",
+              }}
+            >
+              <Box 
+                component={"img"}
+                src={ogImage}
+                alt={"profile"}
+                height={"32px"}
+                width={"32px"}
+                borderRadius={"50%"}
+                sx={{objectFit: "cover"}}
+              />
+              <Box textAlign={"left"}>
+                  <Typography fontWeight={"bold"} fontSize={"0.85rem"} sx={{color: theme.palette.secondary[100]}} >
+                    {user.name}
+                  </Typography>
+                  <Typography fontSize={"0.75rem"} sx={{color: theme.palette.secondary[200]}} >
+                    {user.occupation}
+                  </Typography>
+                </Box>
+                <ArrowDropDownOutlined 
+                    sx={{
+                      color: theme.palette.secondary[300],
+                      fontSize: "25px",
+                    }}
+                  />
+            </Button>
+            <Menu anchorEl={anchorEl} open={isOpen} onClose={handleClose} anchorOrigin={{vertical:"bottom", horizontal:"center"}}>
+              <MenuItem onClick={handleClose}>Log Out</MenuItem>
+            </Menu>
+          </FlexBetween>
         </FlexBetween>
       </Toolbar>
     </AppBar>
   );
 }
+
+Navbar.propTypes = {
+  user: PropTypes.object.isRequired,
+  isSidebarOpen: PropTypes.bool.isRequired,
+  setIsSidebarOpen: PropTypes.func.isRequired,
+};
 
 export default Navbar;
