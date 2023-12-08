34a63eb9fcf84a8cef55c4843ce2b1c711c13653
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-08 21:04:20 +0530

    showing the profile image of user and user name and occupation after fetching user details from the backend database using RTK query api call in the backend

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -35,6 +35,8 @@ import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import FlexBetween from "./FlexBetween";
 import profileImage from "../assets/unsplash.jpg";
+import ogImage from "../assets/profile.jpeg";
+import PropTypes from "prop-types";
 
 // creating a list/array of navItems
 const navItems = [
@@ -97,6 +99,7 @@ const navItems = [
 ];
 
 function Sidebar({
+  user,
   drawerWidth,
   isSidebarOpen,
   setIsSidebarOpen,
@@ -197,10 +200,42 @@ function Sidebar({
               })}
             </List>
           </Box>
+          <Box position={"absolute"} bottom={"2rem"}>
+            <Divider />
+            <FlexBetween textTransform={"none"} gap={"1rem"} m={"1.5rem 2rem 0 3rem"}>
+              <Box 
+                component={"img"}
+                src={profileImage}
+                alt={"profile"}
+                height={"40px"}
+                width={"40px"}
+                borderRadius={"50%"}
+                sx={{objectFit: "cover"}}
+              />
+                <Box textAlign={"left"}>
+                  <Typography fontWeight={"bold"} fontSize={"0.9rem"} sx={{color: theme.palette.secondary[100]}} >
+                    {user.name}
+                  </Typography>
+                  <Typography fontSize={"0.8rem"} sx={{color: theme.palette.secondary[200]}} >
+                    {user.occupation}
+                  </Typography>
+                </Box>
+                <SettingsOutlined 
+                  sx={{
+                    color: theme.palette.secondary[300],
+                    fontSize: "25px",
+                  }}
+                />
+            </FlexBetween>
+          </Box>
         </Drawer>
       )}
     </Box>
   );
 }
+
+Sidebar.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default Sidebar;
