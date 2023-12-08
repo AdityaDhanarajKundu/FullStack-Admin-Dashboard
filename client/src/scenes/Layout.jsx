5c8323b2b45ae16736890e4d4e0d6fdffcb83838
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-08 20:29:10 +0530

    fetching the user using the userId in the redux toolkit store from the backend and console logging it to see check the output

--- client/src/scenes/Layout.jsx
+++ client/src/scenes/Layout.jsx
@@ -4,12 +4,20 @@ import {Outlet} from "react-router-dom";
 import {useSelector} from "react-redux";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import { useGetUserQuery } from "../state/api";
 
 function Layout(){
     const isNonMobile = useMediaQuery("(min-width: 600px)"); 
 
     // state values to toggle the sidebar 
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+    // get the user id state value from the global state in redux store
+    const userId = useSelector((state)=> state.global.userId);
+
+    // making the api call to retrieve the user data using the useGetUserQuery hook RTKQ
+    const {data} = useGetUserQuery(userId);
+    console.log("data",data);
     
     return(
         <Box display={isNonMobile ? "flex" : "block"} width={"100%"} height={"100%"}>
