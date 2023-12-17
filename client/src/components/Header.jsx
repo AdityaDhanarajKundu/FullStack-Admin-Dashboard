ac39097c416668c0bc23f4e622e6ae56b23a2457
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-17 17:53:53 +0530

    header component to add/show the header for each page

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+import { Typography, Box, useTheme } from "@mui/material";
+
+function Header({ title, subtitle }) {
+  const theme = useTheme();
+
+  return (
+    <Box>
+      <Typography
+        variant="h2"
+        color={theme.palette.secondary[100]}
+        fontWeight="bold"
+        sx={{ marginBottom: "5px" }}
+      >
+        {title}
+      </Typography>
+      <Typography variant="h2" color={theme.palette.secondary[100]}>
+        {subtitle}
+      </Typography>
+    </Box>
+  );
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+};
+
+export default Header;
