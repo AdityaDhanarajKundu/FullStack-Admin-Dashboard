7a3e187dfe8ccf70bc03a06c6ebfa0cd03205b6e
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-17 19:00:05 +0530

    made the typography structure for the product card content need to add the card actions

--- client/src/scenes/Products.jsx
+++ client/src/scenes/Products.jsx
@@ -12,9 +12,77 @@ import {
   useMediaQuery,
 } from "@mui/material";
 import { useGetProductsQuery } from "../state/api";
+import Header from "../components/Header";
+
+// Product component for each individual product rather than creating the component seperately
+function Product({
+    _id,
+    name,
+    description,
+    price,
+    rating,
+    category,
+    supply,
+    stat
+}){
+    const theme = useTheme();
+    const [isExpanded, setIsExpanded] = useState(false);  // to toggle the see more button
+    
+    return(
+        <Card
+            sx={{
+                backgroundImage: "none",
+                backgroundColor: theme.palette.background.alt,
+                borderRadius: "0.55rem",
+            }}
+        >
+            <CardContent>
+                <Typography sx={{fontSize : 14}} color={theme.palette.secondary[700]} gutterBottom >
+                    {category}
+                </Typography>
+                <Typography variant="h5" component={"div"} >
+                    {name}
+                </Typography>
+                <Typography sx={{mb : "1.5rem"}} color={theme.palette.secondary[400]} >
+                    ${Number(price).toFixed(2)}
+                </Typography>
+                <Rating value={rating} readOnly />
+
+                <Typography variant="body2">{description}</Typography>
+            </CardContent>
+        </Card>
+    );
+}
 
 function Products() {
-  return <div>This is a products page.</div>;
+  const {data, isLoading} = useGetProductsQuery();  
+
+  const isNonMobile = useMediaQuery("(min-width: 1000px)");
+  
+  console.log("data", data);  // should show list of 30 products from the database
+    
+  return (
+    <Box margin={"1.5rem 2.5rem"}>
+      <Header title="PRODUCTS" subtitle="See your list of products" />
+      {data || !isLoading ? (
+        <Box 
+            mt={"20px"}
+            display={"grid"}
+            gridTemplateColumns={"repeat(4, minmax(0, 1fr))"}
+            justifyContent={"space-between"}
+            rowGap={"20px"}
+            columnGap={"1.33%"}
+            sx={{
+                "& > div": {gridColumn: isNonMobile ? undefined : "span 4"} // using media query to change the grid column span and making it responsive for all devices
+            }}
+        >
+            {data.map()}
+        </Box>
+      ):(
+        <>Loading...</>
+      )}
+    </Box>
+  );
 }
 
 export default Products;
