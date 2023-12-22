297b45aeb02c83ee426d3e52af6dcd84fe008c06
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-22 13:52:27 +0530

    created the columns and set the margin of the parent box and limited the height of the datagrid box

--- client/src/scenes/Customers.jsx
+++ client/src/scenes/Customers.jsx
@@ -1,8 +1,68 @@
-
+import {Box, useTheme} from "@mui/material";
+import { useGetCustomersQuery } from "../state/api";
+import Header from "../components/Header";
+import { DataGrid } from "@mui/x-data-grid";
 
 function Customers(){
+
+    const theme = useTheme(); // object of useTheme 
+    const {data, isLoading} = useGetCustomersQuery();
+    console.log("data", data);
+    
+    //creating the columns for the datagrid
+    const columns = [
+        {
+            field: "_id", // this refers to the field in the data base
+            headerName: "ID", // this is the column name
+            flex: 1,
+        },
+        {
+            field: "name",
+            headerName: "Name",
+            flex: 0.5,
+        },
+        {
+            field: "email",
+            headerName: "Email",
+            flex: 1,
+        },
+        {
+            field: "phoneNumber",
+            headerName: "Phone Number",
+            flex: 1,
+            renderCell: (params)=> {
+                return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, "($1)$2-$3")
+            }
+        },
+        {
+            field: "country",
+            headerName: "Country",
+            flex: 0.4,
+        },
+        {
+            field: "occupation",
+            headerName: "Occupation",
+            flex: 1,
+        },
+        {
+            field: "role",
+            headerName: "Role",
+            flex: 0.5,
+        }
+    ];
+    
     return(
-        <div>This is a customers page</div>
+        <Box m={"1.5rem 2.5rem"}>
+            <Header title="CUSTOMERS" subtitle="List of Customers" />
+            <Box mt={"40px"} height={"75vh"}>
+                <DataGrid 
+                    loading={isLoading || !data}
+                    getRowId={(row)=> row._id}
+                    rows={data || []}
+                    columns={columns}
+                />
+            </Box>
+        </Box>
     );
 }
 
