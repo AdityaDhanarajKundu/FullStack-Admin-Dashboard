8c3de4891a49d1745a6a6cd5f6a94a85f5186845
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-23 00:22:17 +0530

    sending the transaction pagination handling function to the frontend

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,30 +1,69 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
 
 export async function getProducts(req, res) {
-    try{
-        const products = await Product.find();
+  try {
+    const products = await Product.find();
 
-        // grabbing the product stats
-        const productsWithStats = await Promise.all(products.map(async (product) => {
-            const stat = await ProductStat.find({ product: product._id });
-            return { ...product._doc, stat };
-        }));
+    // grabbing the product stats
+    const productsWithStats = await Promise.all(
+      products.map(async (product) => {
+        const stat = await ProductStat.find({ product: product._id });
+        return { ...product._doc, stat };
+      })
+    );
 
-        res.status(200).json(productsWithStats); // show the products stats upon get request response and the set the get request response status as 200
-    }
-    catch(error){
-        res.status(404).json({ message : error.message });
-    }
+    res.status(200).json(productsWithStats); // show the products stats upon get request response and the set the get request response status as 200
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 }
 
-export async function getCustomers(req, res){
-    try{
-        const customers = await User.find({role: "user"}).select("-password");
-        res.status(200).json(customers);
+export async function getCustomers(req, res) {
+  try {
+    const customers = await User.find({ role: "user" }).select("-password");
+    res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
+export async function getTransactions(req, res) {
+  try {
+    // client side pagination
+
+    // sort should look like this: { "field": "userId", "sort": "desc"}
+    const { page = 1, pageSize = 10, sort = null, search = "" } = req.query;
+
+    // formatted sort should look like { userId: -1 }
+    function generateSort() {
+      const sortParsed = JSON.parse(sort);
+      const sortFormatted = {
+        [sortParsed.field]: sortParsed.sort === "asc" ? 1 : -1,
+      };
+      return sortFormatted;
     }
-    catch(error){
-        res.status(404).json({ message : error.message });
-    }
+    const sortFormatted = Boolean(sort) ? generateSort() : null;
+
+    const transactions = await Transaction.find({
+      $or: [
+        { cost: { $regex: new RegExp(search, "i") } },
+        { userId: { $regex: new RegExp(search, "i") } },
+      ],
+    })
+      .sort(sortFormatted)
+      .skip(page * pageSize)
+      .limit(pageSize);
+
+    // to display the complete transaction data so that it doesnt get affected by pagination
+    const total = await Transaction.countDocuments({
+        name: {$regex: search, $options: "i"}
+    }); 
+
+    res.status(200).json({ transactions, total});
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 }
