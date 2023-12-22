00f50b76fc2e5e169047d7f3aac374f2ce24ddef
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-23 00:21:50 +0530

    importing the controller function for the transaction

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,9 +1,11 @@
 import express, { Router } from 'express';
-import { getProducts, getCustomers } from "../controllers/client.js";
+import { getProducts, getCustomers, getTransactions } from "../controllers/client.js";
+import { get } from 'mongoose';
 
 const router = express.Router();
 
 router.get('/products', getProducts);
 router.get('/customers', getCustomers);
+router.get("/transactions", getTransactions);
 
 export default router;
