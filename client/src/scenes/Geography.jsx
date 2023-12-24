7e11c745f856a80332cf6b4e60702810aab02f52
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-24 13:20:53 +0530

    route to geography frontend component

---
+++ client/src/scenes/Geography.jsx
@@ -0,0 +1,13 @@
+import {Box, useTheme} from "@mui/material";
+import { useGetGeographyQuery } from "../state/api";
+import Header from "../components/Header";
+
+function Geography() {
+  return (
+    <div>
+      <h1>Geography</h1>
+    </div>
+  );
+}
+
+export default Geography;
