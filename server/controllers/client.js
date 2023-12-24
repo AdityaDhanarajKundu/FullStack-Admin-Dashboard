5b154de97d74e4e757b6f8d8900b770244cc855d
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-24 10:22:50 +0530

    geography controller to get the three letter country code from user collections

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -2,6 +2,7 @@ import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
 import Transaction from "../models/Transaction.js";
+import getCountryIso3 from "country-iso-2-to-3";
 
 export async function getProducts(req, res) {
   try {
@@ -64,6 +65,31 @@ export async function getTransactions(req, res) {
 
     res.status(200).json({ transactions, total});
   } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
+export async function getGeography(req, res){
+  try{
+    const users = await User.find();
+    const mappedLocations = users.reduce((acc, {country})=>{
+      const countryISO3 = getCountryIso3(country);
+      if(!acc[countryISO3]){
+        acc[countryISO3] = 0;
+      }
+      acc[countryISO3]++;
+      return acc;
+    },{});
+
+    const formattedLocations = Object.entries(mappedLocations).map(
+      ([country, count])=>{
+        return {id: country, value: count}
+      }
+    );
+
+    res.status(200).json(formattedLocations);
+  }
+  catch(error){
     res.status(404).json({ message: error.message });
   }
 }
