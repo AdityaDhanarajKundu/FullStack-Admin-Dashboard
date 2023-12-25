c405459060bd47f7d68d3841b04a4373030713e7
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-26 01:53:56 +0530

    made the overview overall sales and units plot using the nivo charts

--- client/src/scenes/Overview.jsx
+++ client/src/scenes/Overview.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+import {FormControl, MenuItem, InputLabel, Box, Select} from "@mui/material";
+import Header from "../components/Header";
+import OverviewCharts from "../components/OverviewCharts";
+
 function Overview(){
+
+    const [view, setView] = useState("units");
+    
     return(
-        <div>
-            <h1>Overview</h1>
-        </div>
+        <Box m={"1.5rem 2.5 rem"}>
+            <Header title="OVERVIEW" subtitle="General revenue and profit overview" />
+            <Box height={"75vh"}>
+                <FormControl sx={{mt: "1rem"}} >
+                    <InputLabel>View</InputLabel>
+                    <Select value={view} label="View" onChange={(e)=> setView(e.target.value)}>
+                        <MenuItem value="sales">Sales</MenuItem>
+                        <MenuItem value="units">Units</MenuItem>
+                    </Select>
+                </FormControl>
+                <OverviewCharts view={view} />
+            </Box>
+        </Box>
     );
 }
 
