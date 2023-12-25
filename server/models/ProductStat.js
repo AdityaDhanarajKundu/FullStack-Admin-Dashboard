ac25e9a8d9f9c459001787db5078d7530bf4bd2b
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-26 01:03:39 +0530

    array of objects

--- server/models/ProductStat.js
+++ server/models/ProductStat.js
@@ -1,27 +1,29 @@
 // file to create the mongoose schema for the user model in our Mongo Database
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 // schema() is constructor method to create a new Schema class instance/object
 const ProductStatSchema = mongoose.Schema(
-    {
-        productId : String,
-        yearlySalesTotal : Number,
-        yearlyTotalSoldUnits : Number,
-        year : Number,
-        monthlyData : [
-            {
-                month: String,
-                totalSales: Number,
-                totalUnits: Number,
-            }
-        ],
-        dailyData : {
-            data: String,
-            totalSales: Number,
-            totalUnits: Number,
-        },
-    },
-    { timestamps : true } // this enables the timestamp feature for the schema to track the changes and modifications in the schema
+  {
+    productId: String,
+    yearlySalesTotal: Number,
+    yearlyTotalSoldUnits: Number,
+    year: Number,
+    monthlyData: [
+      {
+        month: String,
+        totalSales: Number,
+        totalUnits: Number,
+      },
+    ],
+    dailyData: [
+      {
+        data: String,
+        totalSales: Number,
+        totalUnits: Number,
+      },
+    ],
+  },
+  { timestamps: true } // this enables the timestamp feature for the schema to track the changes and modifications in the schema
 );
 
 // create the mongoose model
