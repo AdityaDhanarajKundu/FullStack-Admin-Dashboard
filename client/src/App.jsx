7c7e4daaccdd248d92d11ce9d880c61e6887a674
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-26 18:22:12 +0530

    new file and route for the breakdown page component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import Geography from "./scenes/Geography";
 import Overview from './scenes/Overview';
 import Daily from './scenes/Daily';
 import Monthly from './scenes/Monthly';
+import Breakdown from './scenes/Breakdown';
 
 function App() {
 
@@ -36,6 +37,7 @@ function App() {
               <Route path='/overview' element={<Overview />} />
               <Route path='/daily' element={<Daily />} />
               <Route path='/monthly' element={<Monthly />} />
+              <Route path='/breakdown' element={<Breakdown />} />
             </Route>
           </Routes>
         </ThemeProvider>
