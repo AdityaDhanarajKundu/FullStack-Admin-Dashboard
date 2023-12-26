55ffebd212701ae94bf3a5fb4caf20af33e05cc5
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-26 19:24:34 +0530

    find the user with role of admin from the mongodb and deselecting the password field

---
+++ server/controllers/management.js
@@ -0,0 +1,11 @@
+import User from "../models/User.js";
+
+export async function getAdmin(req, res){
+    try{
+        const admins = await User.find({role: "admin"}).select("-password");
+        res.status(200).json(admins);
+    }
+    catch(error){
+        res.status(404).json({message: error.message});
+    }
+}
