53db5a353452ecd20cd73ac4fa4f81bd888942d3
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-27 13:06:51 +0530

    changed the columns

--- client/src/scenes/Performance.jsx
+++ client/src/scenes/Performance.jsx
@@ -18,43 +18,33 @@ function Performance() {
       flex: 1,
     },
     {
-      field: "name",
-      headerName: "Name",
-      flex: 0.5,
-    },
-    {
-      field: "email",
-      headerName: "Email",
+      field: "userId",
+      headerName: "User ID",
       flex: 1,
     },
     {
-      field: "phoneNumber",
-      headerName: "Phone Number",
-      flex: 0.5,
-      renderCell: (params) => {
-        return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, "($1)$2-$3");
-      },
-    },
-    {
-      field: "country",
-      headerName: "Country",
-      flex: 0.4,
-    },
-    {
-      field: "occupation",
-      headerName: "Occupation",
+      field: "createdAt",
+      headerName: "CreatedAt",
       flex: 1,
     },
     {
-      field: "role",
-      headerName: "Role",
+      field: "products",
+      headerName: "# of Products",
       flex: 0.5,
+      sortable: false,
+      renderCell: (params) => params.value.length,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
     },
   ];
 
   return (
     <Box m="1.5rem 2.5rem">
-      <Header title="ADMINS" subtitle="Managing admins and list of admins" />
+      <Header title="PERFORMANCE" subtitle="Track Your Affiliate Sales Performance" />
       {/* <Box
         mt="40px"
         height="75vh"
